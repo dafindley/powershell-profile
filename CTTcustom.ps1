30dd9ff67b9b02daa69c20fb8111d4dc99ff9ea1
David Findley <dafindley@gmail.com>
2026-09-16 22:15:54 +1000

    Update CTTcustom.ps1

--- CTTcustom.ps1
+++ CTTcustom.ps1
@@ -1,1 +1,19 @@
+$debug_Override = $null
+$repo_root_Override = $null
+$timeFilePath_Override = $null
+$updateInterval_Override = $null
+$show_help_Override = $null
+$EDITOR_Override = $null
+
 $env:POSH_THEME = "$HOME\Documents\GitHub\powershell-profile\themes\jandedobbeleer.omp.json"
+. "$HOME\Documents\GitHub\powershell-scripts\Invoke-SPOVersionTrim.ps1"
+
+Set-PSReadLineKeyHandler `
+    -Chord 'Ctrl+Shift+t' `
+    -BriefDescription 'SPOVersionTrim' `
+    -LongDescription 'Open SharePoint Version Trim Utility' `
+    -ScriptBlock {
+        [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
+        [Microsoft.PowerShell.PSConsoleReadLine]::Insert('Invoke-SPOVersionTrim')
+        [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
+    }
