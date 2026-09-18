6d789df3570c68eb13be3d754e3f56be44149f05
David Findley <dafindley@gmail.com>
2026-09-18 10:50:40 +1000

    custom profile updated

--- CTTcustom.ps1
+++ CTTcustom.ps1
@@ -7,18 +7,19 @@ $EDITOR_Override = $null
 
 $env:POSH_THEME = "$HOME\Documents\GitHub\powershell-profile\themes\jandedobbeleer.omp.json"
 . "$HOME\Documents\GitHub\powershell-scripts\Invoke-SPOVersionTrim.ps1"
+. "$HOME\Documents\GitHub\powershell-scripts\New-SPOTrimTargetCsv.ps1"
 
-Set-PSReadLineKeyHandler `
-    -Chord 'Ctrl+Shift+t' `
-    -BriefDescription 'SPOVersionTrim' `
-    -LongDescription 'Open SharePoint Version Trim Utility' `
-    -ScriptBlock {
-        [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
-        [Microsoft.PowerShell.PSConsoleReadLine]::Insert('Invoke-SPOVersionTrim')
-        [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
-    }
+# Set-PSReadLineKeyHandler `
+#     -Chord 'Ctrl+Shift+t' `
+#     -BriefDescription 'SPOVersionTrim' `
+#     -LongDescription 'Open SharePoint Version Trim Utility' `
+#     -ScriptBlock {
+#         [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
+#         [Microsoft.PowerShell.PSConsoleReadLine]::Insert('Invoke-SPOVersionTrim')
+#         [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
+#     }
+
 # SharePoint Version Trim Utility
 function spotrim {
     Invoke-SPOVersionTrim @args
 }
-
